af3102de71ac9ae79fb141467bf80d279f32d43a
minkalai <renae.reid16@gmail.com>
2026-03-22 18:21:40 -0600

    trying to set up the attack and dammage taking

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    public Enemy enemy;
+    private Enemy enemy;
     int dammage;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
