ef9018c820ddcd89e05c7db8258e3e4541bac836
minkalai <renae.reid16@gmail.com>
2026-03-24 14:56:33 -0600

    Update Attack.cs

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -14,6 +14,8 @@ public class Attack : MonoBehaviour
 	{
         Debug.Log("Attack triggered");
 		if (collision.CompareTag("Enemy"))
+
+
         {
             enemy = collision.GetComponent<Enemy>();
             Debug.Log("Enemy Hit");
